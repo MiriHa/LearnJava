d66396258a93c2e56d1f9d566cfc25a4aeb81737
Miri <miriam.halsner.99@gmail.com>
2019-12-01 16:42:15 +0100

    Some fragmentslogic added

--- app/src/main/java/com/example/learnjava/Controller.java
+++ app/src/main/java/com/example/learnjava/Controller.java
@@ -2,5 +2,33 @@ package com.example.learnjava;
 
 import android.app.Application;
 
+import com.example.learnjava.models.ModelExercise;
+import com.example.learnjava.models.ModelLesson;
+import com.example.learnjava.models.ModelUserProgress;
+
 public class Controller extends Application {
+
+    ModelUserProgress modelUserProgress;
+
+    //TODO may this instead of user model?
+    // ArrayList<ModelExercise> exerciseList = new ArrayList<>();
+
+
+    public ModelUserProgress getUserProgresModel() {
+        return modelUserProgress;
+    }
+
+
+    //do I need these?
+    public void addFinishedExercise(ModelExercise exercise){
+        modelUserProgress.addFinishedExercise(exercise);
+    }
+
+    public void addReadLesson(ModelLesson lesson){
+        modelUserProgress.addReadLesson(lesson);
+    }
+
+    public void updateFinishedSection(int number){
+        modelUserProgress.updateUserProgressSections(number);
+    }
 }

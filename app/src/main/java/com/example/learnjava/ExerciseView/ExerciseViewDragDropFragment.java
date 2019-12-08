5e6a28b2d0f5741871922001b86a4c6273539df8
Miri <miriam.halsner.99@gmail.com>
2019-12-09 00:08:23 +0100

    Drag and drop view begonnen.

--- app/src/main/java/com/example/learnjava/ExerciseView/ExerciseViewDragDropFragment.java
+++ app/src/main/java/com/example/learnjava/ExerciseView/ExerciseViewDragDropFragment.java
@@ -11,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.learnjava.ExerciseCommunication;
@@ -23,7 +25,7 @@ import com.example.learnjava.models.ModelTask;
  * ViewType: 4
  */
 
-public class ExerciseViewDragDropFragment extends Fragment implements View.OnLongClickListener, View.OnDragListener{
+public class ExerciseViewDragDropFragment extends Fragment implements View.OnLongClickListener, View.OnDragListener {
 
     private ExerciseCommunication mListener;
 
@@ -31,7 +33,7 @@ public class ExerciseViewDragDropFragment extends Fragment implements View.OnLon
     ModelTask currentTask;
 
     private Button nextButton;
-
+    private LinearLayout contentHolder, answerHolder;
 
     public ExerciseViewDragDropFragment() {
         // Required empty public constructor
@@ -51,12 +53,16 @@ public class ExerciseViewDragDropFragment extends Fragment implements View.OnLon
         //get the currentTask
         receiveCurrentTask();
 
-        TextView exerciseText = view.findViewById(R.id.exerciseTextAnswer);
-        nextButton = view.findViewById(R.id.nextButtonExerciseAnswer);
+        contentHolder = view.findViewById(R.id.contentHolderDragDrop);
+        answerHolder = view.findViewById(R.id.answerHolderDragDrop);
+
+        setContentLayout();
+
+        nextButton = view.findViewById(R.id.nextButtonExerciseDragDrop);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("BUTTONCLICKED", " in AnswerView");
+                Log.i("BUTTONCLICKED", " in DragDropView");
                 checkAnswers();
             }
         });
@@ -64,6 +70,45 @@ public class ExerciseViewDragDropFragment extends Fragment implements View.OnLon
 
         return view;
     }
+
+    private void setContentLayout() {
+
+        //Arrag length needs to bedivisble through 3 -> per row textview dropView textview
+        //ansosten zweidimensionales array? verschachteltes array?
+        String[] contentArray = currentTask.getContentStringArray();
+
+        LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams mParamsWeight = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, (float) 1.0);
+
+
+        for (int i = 0; i < contentArray.length; i++) {
+
+            LinearLayout holder = new LinearLayout(getContext());
+            holder.setLayoutParams(mParams);
+            holder.setOrientation(LinearLayout.HORIZONTAL);
+
+            if(contentArray[i].equals("")){
+                TextView textView1 = new TextView((getContext()));
+                textView1.setLayoutParams(mParams);
+                textView1.setText(contentArray[i]);
+                textView1.setTag("dropView" + i);
+                Log.i("M DRAGDROP", " content: " + i + " " + contentArray[i] );
+                holder.addView(textView1);
+            }else {
+                TextView textView1 = new TextView((getContext()));
+                textView1.setLayoutParams(mParams);
+                textView1.setText(contentArray[i]);
+                Log.i("M DRAGDROP", " content: " + i + " " + contentArray[i] );
+                holder.addView(textView1);
+            }
+
+            if ((i+1) % 3 == 0) {
+                Log.i("M DRAGDROP modulo", " content: " + i + " " + contentArray[i] );
+                contentHolder.addView(holder);
+            }
+        }
+
+}
 
     @Override
     public void onAttach(Context context) {

5f55819d827a6885b84b28230e26d7b5ae4687a3
Miri <miriam.halsner.99@gmail.com>
2019-12-28 23:06:21 +0100

    ExerciseView Code finshed but not tested, ExerciseView Order begun

--- app/src/main/java/com/example/learnjava/exercise_view/ExerciseViewCodeFragment.java
+++ app/src/main/java/com/example/learnjava/exercise_view/ExerciseViewCodeFragment.java
@@ -9,12 +9,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.learnjava.Controller;
 import com.example.learnjava.ExerciseCommunication;
 import com.example.learnjava.R;
 import com.example.learnjava.models.ModelTask;
+import com.example.learnjava.room_database.UserDatabase;
+
+import java.util.Calendar;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 
 /**
@@ -24,94 +34,122 @@ import com.example.learnjava.models.ModelTask;
 
 public class ExerciseViewCodeFragment extends Fragment {
 
-    private ExerciseCommunication mListener;
+
+        private ExerciseCommunication mListener;
 
 
-    ModelTask currentTask;
+        private ModelTask currentTask;
+        private Controller progressController;
+        private UserDatabase database;
 
-    private Button nextButton;
-
-
-    public ExerciseViewCodeFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_exercise_view_code, container, false);
-
-        //get the currentTask
-        receiveCurrentTask();
-
-        TextView exerciseText = view.findViewById(R.id.exerciseTextAnswer);
-        nextButton = view.findViewById(R.id.nextButtonExerciseAnswer);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("BUTTONCLICKED", " in AnswerView");
-                checkAnswers();
-            }
-        });
+        private EditText answerEditText;
 
 
-        return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        try {
-            mListener = (ExerciseCommunication) getParentFragment();
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement ExerciseCommunication");
+        public ExerciseViewCodeFragment() {
+            // Required empty public constructor
         }
 
-        super.onAttach(context);
-    }
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+        }
 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_exercise_view_code, container, false);
+            progressController = (Controller) getActivity().getApplicationContext();
 
-    private void checkAnswers() {
-//
-//        Log.i("CheckAnswers", " anser: " + userInput + " solution: " + currentTask.getSolutionString());
-//        if (currentTask.getSolutionString().equals(userInput)) {
-//            mListener.sendAnswerFromExerciseView(true);
-//            Log.i("SENDANSWERFROMEXERCISE", " answer: true");
-//            Toast.makeText(getContext(), "Answer was right.", Toast.LENGTH_SHORT).show();
-//        } else {
-//            Log.i("ANSWER", " was wrong");
-//            mListener.sendAnswerFromExerciseView(false);
-//            Log.i("SENDANSWERFROMEXERCISE", " answer: false");
-//            Toast.makeText(getContext(), "Answer was false", Toast.LENGTH_SHORT).show();
-//            //TODO give feedback that false answer??
-//        }
-    }
-
-    public void setExerciseCommunication(ExerciseCommunication callback) {
-        Log.d("SETEXERCISECOMM", " setMlistenere");
-        this.mListener = callback;
-    }
+            //get the currentTask
+            receiveCurrentTask();
+            database = UserDatabase.getInstance(getContext());
 
 
-    private void receiveCurrentTask() {
-        this.currentTask = mListener.sendCurrentTask();
-    }
+            TextView exerciseText = view.findViewById(R.id.exerciseTextCode);
+            exerciseText.setText(currentTask.getTaskText());
+            answerEditText = view.findViewById(R.id.CodeTextInput);
+            Button nextButton = view.findViewById(R.id.nextButtonExerciseCode);
+
+            if(progressController.checkTasks(currentTask)) {
+                Log.i("M_Exercise_VIEW_CODE", "checkExericse and skip");
+                nextButton.setText(R.string.Skip);
+            }
+            nextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("M_EXERCISE_VIEW_CODE", "Nextbuttonclicked");
+                    checkAnswers();
+                    //Hide the Keyboard
+                    InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(v.getApplicationWindowToken(),0);
+                }
+            });
 
 
-    public  void reset(){
+            return view;
+        }
 
-    }
+        @Override
+        public void onAttach(Context context) {
+            try {
+                mListener = (ExerciseCommunication) getParentFragment();
+            } catch (ClassCastException e) {
+                throw new ClassCastException(context.toString()
+                        + " must implement ExerciseCommunication");
+            }
+
+            super.onAttach(context);
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mListener = null;
+        }
+
+
+        private void checkAnswers() {
+            String userInput = answerEditText.getText().toString();
+            if (progressController.checkTasks(currentTask) && userInput.isEmpty()) {
+                Log.i("M_Exercise_VIEW_CODE", "checkExericse and skip");
+                mListener.justOpenNext();
+                //TODO listnefor textinput, when input change skip to check
+            } else {
+                if (userInput.isEmpty()) {
+                    Toast.makeText(getContext(), "Pleas enter an answer", Toast.LENGTH_SHORT).show();
+                } else {
+                    String userAnswer = userInput.replaceAll("\\s+","");
+                    Log.i("M_EXERCISE_VIEW_CODE", "check answer: " + userInput + " solution: " + currentTask.getSolutionString());
+                    progressController.makeaLog(Calendar.getInstance().getTime(), "EXERCISE_CODE_FRAGMENT", "number: " + currentTask.getTaskNumber() + " userInput: " + userInput, database);
+
+                    if (currentTask.getSolutionString().equalsIgnoreCase(userAnswer)) {
+                        mListener.sendAnswerFromExerciseView(true);
+                        Log.i("M_EXERCISE_VIEW_CODE", " send answer: true");
+                    } else {
+                        Log.i("M ANSWER", " was wrong");
+                        mListener.sendAnswerFromExerciseView(false);
+                        Log.i("M_EXERCISE_VIEW_CODE", "send answer: false");
+                    }
+                }
+            }
+        }
+
+        public void setExerciseCommunication(ExerciseCommunication callback) {
+            Log.d("M_EXERCISE_VIEW_CODE", " setMlistenere");
+            this.mListener = callback;
+        }
+
+
+        private void receiveCurrentTask() {
+            this.currentTask = mListener.sendCurrentTask();
+        }
+
+
+        public void reset(){
+            answerEditText.setText(" ");
+        }
+
+
 }
 
--- app/src/main/java/com/example/learnjava/exercise_view/ExerciseViewOrderFragment.java
+++ app/src/main/java/com/example/learnjava/exercise_view/ExerciseViewOrderFragment.java
@@ -1,118 +1,372 @@
 package com.example.learnjava.exercise_view;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.learnjava.Controller;
 import com.example.learnjava.ExerciseCommunication;
 import com.example.learnjava.R;
 import com.example.learnjava.models.ModelTask;
+import com.example.learnjava.room_database.UserDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
- * This is the View order lines in corret order.
+ * This is the View order lines in correct order.
  * ViewType: 5
  */
 
 public class ExerciseViewOrderFragment extends Fragment {
 
-    private ExerciseCommunication mListener;
+
+    public class ExerciseViewDragDropFragment extends Fragment implements View.OnDragListener, View.OnLongClickListener {
+
+        private ExerciseCommunication mListener;
 
 
-    ModelTask currentTask;
+        private ModelTask currentTask;
+        private Controller progressController;
+        private UserDatabase database;
 
-    private Button nextButton;
+        private String[] answerArray;
+        private ArrayList<String> dropTags = new ArrayList<>();
+        private ArrayList<String> dragTags = new ArrayList<>();
+        private Button nextButton;
+        private LinearLayout contentHolder, answerHolder;
 
-
-    public ExerciseViewOrderFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_exercise_view_order, container, false);
-
-        //get the currentTask
-        receiveCurrentTask();
-
-        TextView exerciseText = view.findViewById(R.id.exerciseTextAnswer);
-        nextButton = view.findViewById(R.id.nextButtonExerciseAnswer);
-
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("BUTTONCLICKED", " in AnswerView");
-                checkAnswers();
-            }
-        });
+        private View currentView;
 
 
-        return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        try {
-            mListener = (ExerciseCommunication) getParentFragment();
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement ExerciseCommunication");
+        public ExerciseViewDragDropFragment() {
+            // Required empty public constructor
         }
 
-        super.onAttach(context);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            final View view = inflater.inflate(R.layout.fragment_exercise_view_drag_drop, container, false);
+            currentView = view;
+            progressController = (Controller) getActivity().getApplicationContext();
+
+
+            //get the currentTask
+            receiveCurrentTask();
+            database = UserDatabase.getInstance(getActivity());
+
+
+            contentHolder = view.findViewById(R.id.contentHolderOrder);
+            answerArray = currentTask.getSolutionStringArray();
+
+            TextView taskText = view.findViewById(R.id.exerciseTextDragDrop);
+            taskText.setText(currentTask.getTaskText());
+
+            setDynamicLayout();
+//        setContentLayout();
+
+            nextButton = view.findViewById(R.id.nextButtonExerciseDragDrop);
+            if(progressController.checkTasks(currentTask)) {
+                Log.i("M_Exercise_VIEW_ORDER", "checkExercise and skip");
+                nextButton.setText(R.string.Skip);
+            }
+            nextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("M_EXERCISE_VIEW_ORDER", "check button pressed");
+                    checkAnswers();
+                }
+            });
+
+
+            final Button resetButton = view.findViewById(R.id.resetButton);
+            resetButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("M_EXERCISE_VIEW_ORDER", "Reset Button pressed");
+                    reset();
+                }
+            });
+
+
+            return view;
+        }
+
+        private void setDynamicLayout() {
+            //Arrag length needs to bedivisble through 3 -> per row textview dropView textview
+            String[] contentArray = currentTask.getContentStringArray();
+
+
+            Log.i("M_EXERCISE_VIEW_ORDER", "setLayout: answerArray: ");
+
+            LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams mParamsWeight = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, (float) 1.0);
+            LinearLayout.LayoutParams mParamsWrap = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            //created the Lines in the ContentHolder
+            for (int i = 0; i < contentArray.length; i++) {
+                //one array element holds the Content of a row
+                String[] textParts = contentArray[i].split("@");
+
+                Log.i("M_EXERCISE_VIEW_ORDER","getContentArray, textParts: " + textParts.toString() + "contentLength: " + contentArray.length);
+
+//                LinearLayout rowHolder = new LinearLayout(getContext());
+//                rowHolder.setLayoutParams(mParamsWrap);
+//                //rowHolder.setPadding(10,0,0,0);
+//                rowHolder.setOrientation(LinearLayout.HORIZONTAL);
+
+                //create the three RowTextview
+                for (int j = 0; j < textParts.length; j++) {
+
+//                    if (textParts[j].equals("#")) {
+//                        TextView textView1 = new TextView((getContext()));
+//                        textView1.setLayoutParams(mParamsWrap);
+//                        textView1.setText("___________");
+//                        textView1.setPadding(4, 12, 4, 12);
+//                        String tag = "dropView" + i + j;
+//                        textView1.setTag(tag);
+//                        dropTags.add(tag);
+//                        textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+//
+//                        textView1.setOnDragListener(this);
+//
+//                        Log.i("M_EXERCISE_VIEW_ORDER", " content: " + j + " " + textParts[j]);
+//                        rowHolder.addView(textView1);
+//                    } else {
+
+//                    LinearLayout textHOlder = new LinearLayout(getContext());
+//                    textHOlder.setLayoutParams(mParams);
+//                    textHOlder.setOrientation(LinearLayout.VERTICAL);
+//                    textHOlder.setTag("DROPVIEW" + j);
+//                    dropTags.add("DROPVIEW" + j);
+
+                    TextView textView1 = new TextView((getContext()));
+                    textView1.setLayoutParams(mParamsWrap);
+                    textView1.setText(textParts[j]);
+                    textView1.setPadding(8, 8, 4, 8);
+                    textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+                    textView1.setTag("DRAGVIEW"+j);
+                    dragTags.add("DRAGVIEW"+j);
+                    Log.i("M_EXERCISE_VIEW_ORDER", " content: " + j + " " + textParts[j]);
+
+//                    textHOlder.addView(textView1);
+                    contentHolder.addView(textView1);
+                }
+            }
+
+        }
+
+
+
+        @Override
+        public void onAttach(Context context) {
+            try {
+                mListener = (ExerciseCommunication) getParentFragment();
+            } catch (ClassCastException e) {
+                throw new ClassCastException(context.toString()
+                        + " must implement ExerciseCommunication");
+            }
+
+            super.onAttach(context);
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mListener = null;
+        }
+
+        //right answers are in an int array saved. The seequence gives the right order and the numbers represent the Strings in
+        //exerciseSolutionIntArray
+
+        private void checkAnswers() {
+
+            if (progressController.checkTasks(currentTask)) {
+                Log.i("M_EXERCISE_VIEW_ORDER", "checkExericse and skip");
+                mListener.justOpenNext();
+            }else {
+                //fetch the Solution and convert to StringArray
+                int[] solutionInt = currentTask.getSolutionIntArray();
+                String[] solutionString = new String[solutionInt.length];
+                for (int i = 0; i < solutionInt.length; i++) {
+                    int place = solutionInt[i];
+                    solutionString[i] = answerArray[place];
+                }
+
+                //fetch the userInput
+                String[] userSolution = new String[solutionInt.length];
+                for (int i = 0; i < solutionInt.length; i++) {
+                    TextView solView = currentView.findViewWithTag(dropTags.get(i));
+                    userSolution[i] = solView.getText().toString();
+                }
+                progressController.makeaLog(Calendar.getInstance().getTime(), "EXERCISE_ORDER_FRAGMENT", "check answers number: " + currentTask.getTaskNumber() + " userInput: " + userSolution, database);
+                //TODO check when no Answer is inputted
+                if (Arrays.equals(solutionString, userSolution)) {
+                    mListener.sendAnswerFromExerciseView(true);
+                    Log.i("M_EXERCISE_VIEW_ORDER", " send answer: true");
+                } else {
+                    Log.i("ANSWER", " was wrong");
+                    mListener.sendAnswerFromExerciseView(false);
+                    Log.i("M_EXERCISE_VIEW_ORDER", " send answer: false");
+                }
+            }
+        }
+
+        public void setExerciseCommunication(ExerciseCommunication callback) {
+            Log.d("SETEXERCISECOMM", " setMlistenere");
+            this.mListener = callback;
+        }
+
+
+        private void receiveCurrentTask() {
+            this.currentTask = mListener.sendCurrentTask();
+        }
+
+
+        public void reset() {
+            for(int i=0; i<dragTags.size();i++) {
+
+                TextView text = currentView.findViewWithTag(dragTags.get(i));
+                text.setVisibility(TextView.VISIBLE);
+            }
+
+            for(int i=0; i<dropTags.size();i++) {
+
+                TextView text = currentView.findViewWithTag(dropTags.get(i));
+                text.setTag(dropTags.get(i));
+                text.setTypeface(Typeface.DEFAULT);
+                text.setText("___________");
+                text.setOnDragListener(this);
+            }
+        }
+
+
+        @Override
+        public boolean onLongClick(View v) {
+
+//            ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+//            String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+//
+//            ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+
+            Log.i("DRAGDROP", "Actiondown touch detected");
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, shadowBuilder, v, 0);
+            v.setVisibility(View.INVISIBLE);
+            return true;
+        }
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (action) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+//                        v.setBackgroundColor(getResources().getColor(R.color.grey));
+//                        v.invalidate();
+                        return true;
+                    }
+                    return false;
+                case DragEvent.ACTION_DRAG_ENTERED:
+//                    v.setBackgroundColor(getResources().getColor(R.color.lightGreen2));
+//                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_EXITED:
+
+                    break;
+                case DragEvent.ACTION_DROP:
+
+                    Log.i("DRAGDROP", "ACTION DROP");
+
+                    View view = (View) event.getLocalState();
+                    //view dragged item is being dropped on
+                    TextView dropTarget = (TextView) v;
+                    //view being dragged and dropped
+                    TextView dropped = (TextView) view;
+                    //checking whether first character of dropTarget equals first character of dropped
+                    if (String.valueOf(dropTarget.getText().toString().charAt(0)).equals("_")){
+//                    if (dropTarget.getText().toString().charAt(0) == dropped.getText().toString().charAt(0)) {
+                        //stop displaying the view where it was before it was dragged
+                        view.setVisibility(View.INVISIBLE);
+                        //update the text in the target view to reflect the data being dropped
+                        dropTarget.setText(dropped.getText().toString());
+                        //make it bold to highlight the fact that an item has been dropped
+                        dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
+                        //if an item has already been dropped here, there will be a tag
+                        Object tag = dropTarget.getTag();
+
+
+                        //if there is already an item here, set it back visible in its original place
+                        Log.i("DRAGDROP", " droptTarget: " + dropTarget.getTag() + " dropped: " + dropped.getTag());
+                        //TODO hANDLE when text is already set
+                        //TODO handle when drop ended not over dropVIew, make that drops can be changed
+                        //TODO check why drag is invisble when noch draged in coorrect drop
+
+//                if (!String.valueOf(dropTarget.getText().toString().charAt(1)).equals( "_" )){{
+//                            //the tag is the view id already dropped here
+//                            String existingID = (String) tag;
+//                            //set the original view visible again
+//                            view.findViewWithTag(existingID).setVisibility(View.VISIBLE);
+//                        }
+                        //set the tag in the target view being dropped on - to the Tag of the view being dropped
+                        //TODO is this needed? answer fetching with old Tag would be better
+                        //dropTarget.setTag(dropped.getTag());
+                        view.setVisibility(View.INVISIBLE);
+                        //remove setOnDragListener by setting OnDragListener to null, so that no further drag & dropping on this TextView can be done
+                        dropTarget.setOnDragListener(null);
+                        return true;
+                    }
+
+                    else{
+
+                        Log.i("DRAGDROP", "was not droped correctly");
+                        view.setVisibility(View.VISIBLE);
+                    }
+
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("DRAGDROP", "Drag ended");
+                    Log.i("DRAGDROP", "was not droped correctly");
+                    v.setVisibility(View.VISIBLE);
+
+                    //v.setBackgroundDrawable(normalShape);
+//                default:
+//                    Log.e("DragDrop", "Unknown action type received by OnDragListener.");
+//                    break;
+            }
+            return false;
+        }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
 
-    private void checkAnswers() {
-//        String userInput = editText.getText().toString();
-//        Log.i("CheckAnswers", " anser: " + userInput + " solution: " + currentTask.getSolutionString());
-//        if (currentTask.getSolutionString().equals(userInput)) {
-//            mListener.sendAnswerFromExerciseView(true);
-//            Log.i("SENDANSWERFROMEXERCISE", " answer: true");
-//            Toast.makeText(getContext(), "Answer was right.", Toast.LENGTH_SHORT).show();
-//        } else {
-//            Log.i("ANSWER", " was wrong");
-//            mListener.sendAnswerFromExerciseView(false);
-//            Log.i("SENDANSWERFROMEXERCISE", " answer: false");
-//            Toast.makeText(getContext(), "Answer was false", Toast.LENGTH_SHORT).show();
-//            //TODO give feedback that false answer??
-//        }
-    }
-
-    public void setExerciseCommunication(ExerciseCommunication callback) {
-        Log.d("SETEXERCISECOMM", " setMlistenere");
-        this.mListener = callback;
-    }
 
 
-    private void receiveCurrentTask() {
-        this.currentTask = mListener.sendCurrentTask();
-    }
 
-
-    public void reset(){
-
-    }
 }
 
